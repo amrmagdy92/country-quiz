f8f98c228caeca8b382628d40ca08fe87d4b9532
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2024-04-27 17:49:00 +0300

    rewrote file to ensure all of its dependencies are in one place

--- server/helpers/db.helper.js
+++ server/helpers/db.helper.js
@@ -1,13 +1,34 @@
 import { Sequelize } from "sequelize"
+import dotenv from "dotenv"
 
-const initiateDatabase = (dbHost, dbDialect, dbName, dbUser, dbPass) => {
-    return new Sequelize({
-        host: dbHost,
-        dialect: dbDialect,
-        database: dbName,
-        username: dbUser,
-        password: dbPass
-    })
+dotenv.config()
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    }
+)
+
+sequelize
+    .sync({ alter: true })
+    .then( () => { console.log('Database is now in sync...') })
+    .catch( (err) => { console.log(`Synchornization was not done and the following error was caught:\n${err}`) })
+
+const getErrorMessage = (receivedError) => {
+    const errorMessages = {}
+    for (let i = 0; i < Object.keys(receivedError).length; i++) {
+        const path = receivedError.errors[i].path
+        const msg = receivedError.errors[i].message
+        errorMessages[path] = msg
+    }
+    return errorMessages
 }
 
-export default initiateDatabase
+export {
+    sequelize,
+    getErrorMessage
+}
